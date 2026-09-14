1c636183b19280cdcd3b05ee83ee388c8e35085e
Dina Khedr <dina.khedr@gmail.com>
2026-09-14 21:40:03 +0300

    Update speakers.js

--- aisummit/data/speakers.js
+++ aisummit/data/speakers.js
@@ -9,7 +9,7 @@
  */
 
 const SPEAKER_PHOTO_BASE = "assets/speakers/";
-const HOME_SPEAKER_LIMIT = 12;
+const HOME_SPEAKER_LIMIT = 18;
 
 function getSpeakerPhoto(speaker) {
   if (!speaker) return null;
